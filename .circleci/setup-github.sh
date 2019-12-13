fd93f93e917e6b3cb8f0152e1391b48806cda50d
Kristina Jensen <krj@netcompany.com>
2019-12-13 13:16:55 +0100

    Update circleci

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -4,9 +4,9 @@ git config user.email "$USER_EMAIL"
 git checkout master
 git pull origin master
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv _site/* .
+mv _site/* /docs
 rm -R _site/
+rm -R node_modules
 
 git add -fA
 git commit --allow-empty -m "$(git log -1 --pretty=%B)"
