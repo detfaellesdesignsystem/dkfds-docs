f0114271da1cf8a0fbbd6c85b0824b880e369c0b
Kristina Jensen <krj@netcompany.com>
2020-03-09 15:28:00 +0100

    Fixed cookieprompter in IE

--- javascript/vendor/CookiePrompter.js
+++ javascript/vendor/CookiePrompter.js
@@ -1,3 +1,22 @@
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                if (this.parentNode === null) {
+                    return;
+                }
+                this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 var CookieMgr = (function () {
     var enableLog = false;
     var setCookieOnTopLevelDomain = false;
