4c6d860ba5eadd2d30498d855e1ad1163b519d53
Helle Juhl <hef@netcompany.com>
2024-09-11 21:24:35 +0200

    Updated gen-images script

--- javascript/puppeteer/gen-images.js
+++ javascript/puppeteer/gen-images.js
@@ -1,45 +1,44 @@
 const puppeteer = require('puppeteer');
 
-//var root = "http://127.0.0.1:4000";
+//var root = "http://127.0.0.1:4000/";
 var root = "https://develop.designsystem.dk/";
 var targetRootDir = "img/examples/";
 var themes = ["virk", "borgerdk"];
 
 var exampleUrls = [
-    { "url": "/eksempel/header-simple/", "filename": "header-simple.png", "width": 992, "height": 146 },
-    { "url": "/eksempel/header-with-navigation/", "filename": "header-with-navigation.png", "width": 992, "height": 200 },
-    { "url": "/eksempel/header-switch-language/", "filename": "header-switch-language.png", "width": 992, "height": 180 },
-    { "url": "/eksempel/footer-simple/", "filename": "footer-simple.png", "width": 1017, "height": 114 },
-    { "url": "/eksempel/footer-simple-language/", "filename": "footer-simple-language.png", "width": 1017, "height": 213 },
-    { "url": "/eksempel/footer-three-columns/", "filename": "footer-three-columns.png", "width": 1017, "height": 288 },
-    { "url": "/eksempel/footer-four-columns/", "filename": "footer-four-columns.png", "width": 1017, "height": 328 },
-    { "url": "/eksempel/footer-four-columns-language/", "filename": "footer-four-columns-language.png", "width": 1017, "height": 363 },
-    { "url": "/eksempel/footer-complex/", "filename": "footer-complex.png", "width": 1017, "height": 376 },
-    { "url": "/eksempel/footer-logo/", "filename": "footer-logo.png", "width": 1017, "height": 352 },
-    { "url": "/eksempel/language-switcher/", "filename": "language-switcher.png", "width": 1017, "height": 80 },
-    { "url": "/eksempel/language-switcher-tooltip/", "filename": "language-switcher-tooltip.png", "width": 1017, "height": 95 },
-    { "url": "/eksempel/cookie-message/", "filename": "cookie-message.png", "width": 771, "height": 280 },
-    { "url": "/eksempel/headings/", "filename": "headings.png", "width": 771, "height": 388 },
-    { "url": "/eksempel/skiplink/", "filename": "skiplink.png", "width": 771, "height": 70 },
-    { "url": "/eksempel/back-to-top/", "filename": "back-to-top.png", "width": 300, "height": 60 },
-    { "url": "/eksempel/tabs-navigation/", "filename": "tabs-navigation.png", "width": 771, "height": 286 },
-    { "url": "/eksempel/error-summary-page/", "filename": "error-summary-page.png", "width": 1150, "height": 810 }
+    { "url": "eksempel/header-simple/", "filename": "header-simple.png", "width": 992, "height": 146 },
+    { "url": "eksempel/header-with-navigation/", "filename": "header-with-navigation.png", "width": 992, "height": 200 },
+    { "url": "eksempel/header-switch-language/", "filename": "header-switch-language.png", "width": 992, "height": 180 },
+    { "url": "eksempel/footer-simple/", "filename": "footer-simple.png", "width": 1017, "height": 114 },
+    { "url": "eksempel/footer-simple-language/", "filename": "footer-simple-language.png", "width": 1017, "height": 213 },
+    { "url": "eksempel/footer-three-columns/", "filename": "footer-three-columns.png", "width": 1017, "height": 288 },
+    { "url": "eksempel/footer-four-columns/", "filename": "footer-four-columns.png", "width": 1017, "height": 328 },
+    { "url": "eksempel/footer-four-columns-language/", "filename": "footer-four-columns-language.png", "width": 1017, "height": 363 },
+    { "url": "eksempel/footer-complex/", "filename": "footer-complex.png", "width": 1017, "height": 376 },
+    { "url": "eksempel/footer-logo/", "filename": "footer-logo.png", "width": 1017, "height": 352 },
+    { "url": "eksempel/language-switcher/", "filename": "language-switcher.png", "width": 1017, "height": 80 },
+    { "url": "eksempel/language-switcher-tooltip/", "filename": "language-switcher-tooltip.png", "width": 1017, "height": 95 },
+    { "url": "eksempel/cookie-message/", "filename": "cookie-message.png", "width": 771, "height": 280 },
+    { "url": "eksempel/headings/", "filename": "headings.png", "width": 771, "height": 388 },
+    { "url": "eksempel/skiplink/", "filename": "skiplink.png", "width": 771, "height": 70 },
+    { "url": "eksempel/back-to-top/", "filename": "back-to-top.png", "width": 300, "height": 60 },
+    { "url": "eksempel/tabs-navigation/", "filename": "tabs-navigation.png", "width": 771, "height": 286 },
+    { "url": "eksempel/error-summary-page/", "filename": "error-summary-page.png", "width": 1150, "height": 810 }
 ];
 
 (async () => {
 
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
     for (var t = 0; t < themes.length; t++) {
         for (var i = 0; i < exampleUrls.length; i++) {
+            console.log(root + exampleUrls[i].url + '?theme=' + themes[t]);
 
             var resWidth = exampleUrls[i].width; // width of screenshot
             var resHeight = exampleUrls[i].height;
 
-            const browser = await puppeteer.launch({headless: 'shell'});
-            const page = await browser.newPage();
-
-            console.log(root + exampleUrls[i].url + '?theme=' + themes[t]);
-
-            await page.goto(root + exampleUrls[i].url + '?theme=' + themes[t], { waitUntil: 'networkidle2' });
+            await page.goto(root + exampleUrls[i].url + '?theme=' + themes[t], { waitUntil: 'load' });
             await page.setViewport({ width: resWidth, height: resHeight });
             await page.evaluate(() => matchMedia('screen').matches);
 
@@ -97,7 +96,8 @@ var exampleUrls = [
             var pdfFileName = targetRootDir + '/' + themes[t] + '-' + exampleUrls[i].filename;
 
             await page.screenshot({ path: pdfFileName });
-            await browser.close();
         }
     }
+
+    await browser.close();
 })();
